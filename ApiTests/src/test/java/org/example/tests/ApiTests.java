65a70fa9fd5b40ed79c7676430f3ab3a125d7e93
ankoba <ankoba@cisco.com>
2023-12-26 00:48:42 +0200

    Api Tests

--- ApiTests/src/test/java/org/example/tests/ApiTests.java
+++ ApiTests/src/test/java/org/example/tests/ApiTests.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static io.qameta.allure.SeverityLevel.BLOCKER;
 import static io.restassured.RestAssured.given;
 
 public class ApiTests extends BaseTest {
@@ -26,6 +27,27 @@ public class ApiTests extends BaseTest {
         Root expectedData = buildTestData();
 
         addAttachment("tag.json");
+        Assert.assertEquals(actualResult, expectedData, "Data are not equals");
+    }
+
+    @Step("Run Firt test")
+    @Description("First API within smoke suite")
+    @Epic("First Epic")
+    @Story("First Stroy2")
+    @Feature("First Feature")
+    @Severity(BLOCKER)
+    @Issue("BUG-1234")
+    @TmsLinks(value = {@TmsLink("TC-321"), @TmsLink("TC-322")})
+    @Flaky
+    @Link(name = "Jira", url = "https://www.atlassian.com/?&aceid=&adposition=&adgroup=99178949694&campaign=9869842064&creative=431933496949&device=c&keyword=atlassian&matchtype=e&network=g&placement=&ds_kids=p53276225429&ds_e=GOOGLE&ds_eid=700000001530700&ds_e1=GOOGLE&utm_medium=paid-search&gad_source=1&gclid=Cj0KCQiAj_CrBhD-ARIsAIiMxT9lSfRU0Q5eGM3ZFzqjnBgNZI5mseZ_YYmob97zhFtvs8Cx6buQ3_caAiBjEALw_wcB&gclsrc=aw.ds")
+    @Test
+    public void negativeTest() {
+
+        Root actualResult = executeCall();
+        Root expectedData = buildInvalidTestData();
+
+        addAttachment("screen.png");
+
         Assert.assertEquals(actualResult, expectedData, "Data are not equals");
     }
 
@@ -45,6 +67,23 @@ public class ApiTests extends BaseTest {
         return Root.builder()
                 .data(Data.builder()
                         .id(2)
+                        .email("janet.weaver@reqres.in")
+                        .first_name("Janet")
+                        .last_name("Weaver")
+                        .avatar("https://reqres.in/img/faces/2-image.jpg")
+                        .build())
+                .support(Support.builder()
+                        .url("https://reqres.in/#support-heading")
+                        .text("To keep ReqRes free, contributions towards server costs are appreciated!")
+                        .build())
+                .build();
+    }
+
+    @Step("Build expected Data for Root obj")
+    public Root buildInvalidTestData() {
+        return Root.builder()
+                .data(Data.builder()
+                        .id(3)
                         .email("janet.weaver@reqres.in")
                         .first_name("Janet")
                         .last_name("Weaver")
